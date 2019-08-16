d76c3b28e1eade4d317b543345dfcd4cc59b24b8
Chris Ullyott <contact@chrisullyott.com>
2019-08-16 11:03:22 -0700

    Remove unused helper

--- app/helpers.py
+++ app/helpers.py
@@ -19,10 +19,6 @@ def parse_month_string(string):
 def date_this_month_began():
     return date.today().replace(day=1)
 
-def date_next_month_begins():
-    delta = relativedelta.relativedelta(months=+1)
-    return (date.today() + delta).replace(day=1)
-
 def date_x_month_begins(months):
     delta = relativedelta.relativedelta(months=months)
     return (date_this_month_began() + delta).replace(day=1)
